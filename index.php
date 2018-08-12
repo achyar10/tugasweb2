a51bf5f63ccd3b710331c18d7b7499c3269c66ea
achyar1016 <achyar.anshorie@gmail.com>
2018-08-12 21:45:48 +0700

    templating bootstrap

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Admin</title>
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/custom.css" rel="stylesheet">
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navbar -->
+  <?php include 'navigasi.php'; ?>
+  <!-- Close Navbar -->
+
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include 'sidebar.php'; ?>
+    <!-- Close Sidebar -->
+
+    <div id="content-wrapper">
+      <div class="container-fluid">
+
+        <!-- Main -->
+
+        <!-- Close Main -->
+      </div>
+
+
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © 2018</span>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  </div>
+
+
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,19 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="#">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-fw fa-users"></i>
+      <span>Pengguna</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <a class="dropdown-item" href="login.php">Login</a>
+      
+    </div>
+  </li>
+</ul>
